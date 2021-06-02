85ba7ba08106b19d5de0716f315f490a98940787
damara18 <79318179+damara18@users.noreply.github.com>
2021-06-02 12:55:27 +0700

    add page

---
+++ app/src/main/java/com/garbage/craftivity/ui/home/menu/CraftInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.garbage.craftivity.ui.home.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CraftInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_craft_information)
+    }
+}
